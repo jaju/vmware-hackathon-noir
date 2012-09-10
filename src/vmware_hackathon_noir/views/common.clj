28e64e239062ea0ce82fefd4f7862ff968430778
Ravindra R. Jaju <jaju@msync.org>
2012-09-10 18:59:06 +0530

    Added bootstrap files and made basic changes to common-layout to verify bootstrap working.

--- src/vmware_hackathon_noir/views/common.clj
+++ src/vmware_hackathon_noir/views/common.clj
@@ -1,12 +1,21 @@
 (ns vmware-hackathon-noir.views.common
   (:use [noir.core :only [defpartial]]
-        [hiccup.page :only [include-css html5]]))
+        [hiccup.page :only [include-css include-js html5]]))
 
 (defpartial layout [& content]
             (html5
               [:head
                [:title "vmware-hackathon-noir"]
-               (include-css "/css/reset.css")]
+               (include-css "/css/reset.css")
+               (include-css "/css/bootstrap.css")
+               (include-css "/css/bootstrap-responsive.css")
+               (include-js "/js/bootstrap.js")]
               [:body
-               [:div#wrapper
-                content]]))
+               [:div.container-fluid
+                [:div.navbar
+                 [:div.navbar-inner
+                  [:div.navbar-fixed-top]]]
+                
+                [:div.wrapper
+                 [:div.row-fluid
+                  [:div.span12 content]]]]]))
