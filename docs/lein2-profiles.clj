cadd8d46259313a4ad8dcc46f64c7512c9288948
Ravindra R. Jaju <jaju@msync.org>
2012-09-11 18:36:12 +0530

    Added current leiningen2's profiles.clj to docs.

---
+++ docs/lein2-profiles.clj
@@ -0,0 +1,5 @@
+{:user {:plugins [[lein-immutant "0.9.0"]
+                  [org.clojure/tools.nrepl "0.2.0-beta9"]
+                  [lein-war "0.0.1-SNAPSHOT"]
+                  [lein-ring "0.7.5"]
+                  [lein-noir "1.2.1"]]}}
